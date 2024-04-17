a9f4663fc77062adb99c1b71fa88ec3e9a2801d5
oleh.sten <sten.oleh@gmail.com>
2024-04-17 22:04:19 +0300

    hw 16.04

--- main.js
+++ main.js
@@ -1,26 +1,59 @@
-// 1) Якщо значення змінної number позитивне або 0 - виводимо ''positive", інакше - "negative"
- 
-// 2) Є дві змінні - num1 і num2. Якщо num1>num2 - вивести результат віднімання (num1-num2), інакше - вивести результат множення (num1*num2)
- 
-// 3) Якщо значення змінної кратно(тобто ділиться без остачі) на 7 і одночасно кратно 3 - виводимо "you win!", інакше - "you loose"
-let number = 0;
-if(number =>0){
-    console.log("positive")
-} else {
-    console.log("negative")
+// 1) Написати функцію, яка приймає два значеня і повертає більше значення, якщо значення одного типу, в усіх інших випадках повертає null
+
+
+
+function myFunc(num1, num2){
+    if(typeof(num1) == typeof(num2) && num1>num2){
+        console.log(num1)
+    } else if(typeof(num1) == typeof(num2) && num1<num2){
+        console.log(num2)
+    } else {
+        console.log(null)
+    }
+}
+// 2) Написати функцію, яка просить користувача ввести число і виводить повідомлення яке він число ввів: "додатнє", "нуль" або "від'ємне", або виводить повідомлення "це не число"
+
+
+myFunc(3,2)
+function alertNumberInfo(){
+    // const alertNumber = prompt("Введіть число");
+    const alertNumber = 10;
+    if (alertNumber > 0){
+        alert("number is positive")
+    } else if (alertNumber == 0){
+        alert("number is zero")
+    } else if (alertNumber < 0){
+        alert("number is negative")
+    } else {
+        alert("not a number")
+    }
+
+}
+alertNumberInfo();
+// 3) Написати функцію, яка приймає параметром число і перевіряє чи є це число дільником 100. Повертає булеве значення.
+
+
+
+function getEvenNumber(evenNumber){
+    if(evenNumber%100===0){
+        console.log("true")
+    } else {
+        console.log("false")
+    }
+}
+getEvenNumber(1000)
+
+// 4) Написати функцію, яка перевіряє можливість існування трикутника.
+// Функція приймає три числа - сторони трикутника, повертає булеве значення. Значення за замовчуванням 3,4,5.
+// Трикутник існує тоді і тільки тоді, коли сума довжин будь-яких двох його сторін більша за третю.
+
+
+function isTriangleExist(num1=4, num2=2, num3=1){
+    if((num1+num2) > num3 && (num2+num3) > num1 && (num1+num3) > num2){
+        return true;
+    } else {
+        return false
+    }
 }
 
-const num1 = 6;
-const num2 = 5;
-if(num1>num2){
-    console.log(num1 - num2)
-} else {
-    console.log(num1*num2)
-}
-
-const specialNum = 20;
-if(specialNum%7===0 && specialNum%3===0){
-    console.log('you win')
-} else {
-    console.log('you loose')
-}
+console.log(isTriangleExist())
