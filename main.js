31692496922a07196e0eb4c9ff486b0c86265a8a
oleh.sten <sten.oleh@gmail.com>
2024-04-24 09:48:06 +0300

    hw 22.04 updated

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-// const country = {
+// let country = {
 //     name: "Ukraine",
 //     population: 36000000,
-//     area: 603,
+//     area: 603000,
 //     getDensity: function(){
 //         return 45.78
 //     }
 // }
 
-const country = new Object;
-country.name = "Ukraine";
-country.population = 36000000;
-country.area = 603000;
-country.getDensity = function(){
-    return 45.78;
+function Country(){
+    this.name = name;
+    this.population = population;
+    this.area = area;
+    this.getDensity = function(){
+        return 45.78
+    }
 }
+let NewCountry = new Country("Ukraine", 3800000, 603000)
