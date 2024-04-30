5255bcb1219c4743b929f98c3035bfefdf5ef8e5
oleh.sten <sten.oleh@gmail.com>
2024-04-30 08:11:16 +0300

    25.04 hw

--- main.js
+++ main.js
@@ -1,31 +1,43 @@
-// let numFromUser = prompt("ВВеди тайємне число")
-let countOfTry = 0;
-const secretNum = 7;
-function guessSecretNum(num){
-    countOfTry++;
-    if(!isNaN(num)){
-        if(num > secretNum){
-            console.log("Таємне число менше")
-            let numFromUser = prompt("ВВеди new тайємне число")
-            guessSecretNum(numFromUser)
-        } else if (num < secretNum){
-            console.log("Таємне число більше")
-            let numFromUser = prompt("ВВеди new тайємне число")
-            guessSecretNum(numFromUser)
-        } else {
-            console.log(`Ви вгадали таємне число, кількість спроб ${countOfTry}`)
-        }
-    } else {
-        console.log("введіть число")
-        let numFromUser = prompt("ВВеди new тайємне число")
-        guessSecretNum(numFromUser)
-    }
-}
-// guessSecretNum(numFromUser);
+// Створити об'єкт country двома способами: літерально та за допомогою функції конструктора
+// властивості:
+// - name (рядок)
+// - population (число)
+// - area (число)
+// методи покласти в прототип
+// - getDensity() - повертає число яке є результатом обчислення популяція поділена на площу
 
-const myNumber = 100;
-for(let i = myNumber; i > 0; i--){
-    if(i%9===0){
-        console.log(`Число ${i}`)
+// - getInfo() - повертає рядок з повною інформацією про країну (назва, популяція та площа) з описом що виводить і одиницями вимірювання
+
+// const country = {
+//     name : "Ua",
+//     population : 43000000,
+//     area : 603000,
+// }
+
+// country.__proto__.getDensity = function(){
+//     return this.population/this.area;
+// }
+// country.__proto__.getInfo = function(){
+//     return `${this.name}, ${this.population}, ${this.area}`
+// }
+// console.log(country.getDensity());
+// console.log(country.getInfo())
+
+const countryMethod = {
+    getDensity(){
+        return this.population/this.area;
+    },
+    getInfo(){
+        return `Country ${this.name}, Population${this.population}, Area ${this.area}`
     }
 }
+function getCountry(name, population, area){
+    this.name = name,
+    this.population = population,
+    this.area = area
+}
+
+getCountry.prototype = countryMethod;
+const newCountry = new getCountry("Ua", 43000000, 603000)
+console.log(newCountry.getDensity())
+console.log(newCountry.getInfo())
